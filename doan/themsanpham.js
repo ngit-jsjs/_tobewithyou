be62306b92daff7b75076864482f601c3c8e73fb
ngit-jsjs <152712394+ngit-jsjs@users.noreply.github.com>
2025-05-09 00:05:05 +0700

    Delete doan directory

--- doan/themsanpham.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.add-to-cart').forEach(button => {
-    button.addEventListener('click', function () {
-      alert("Sản phẩm đã được thêm vào giỏ hàng!");
-      // Sau này có thể tích hợp localStorage hoặc gửi lên server ở đây
-    });
-  });
